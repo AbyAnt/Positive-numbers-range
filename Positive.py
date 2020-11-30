2b8136b8c01b2efd91a16dea50d81697de00c040
AbyAnt <73001580+AbyAnt@users.noreply.github.com>
2020-11-30 12:30:14 +0300

    Created by Aby Anthony
    Program to print all positive numbers in a range

---
+++ Positive.py
@@ -0,0 +1,19 @@
+# Python program to print positive Numbers in a List 
+  
+# list of numbers 
+list1 = [12, -7, 5, 64, -14] 
+list2 = [12, 14, -95, 3]
+  
+# iterating each number in list 
+for num1 in list1: 
+    
+# checking condition 
+    if num1 >= 0: 
+       print(num1, end = " ")
+       
+    
+for num2 in list2:
+ 
+    if num2 >= 0:
+       print(num2, end = " ")
+       
